b9c31580598e9a1eb65aafc00cbcf477dcc76beb
Brayanruiz32 <maverick.brm@gmail.com>
2024-06-06 23:26:42 -0500

    actualizacion de la descripcion de la API

--- src/main/java/com/principal/miapi/config/SwaggerConfig.java
+++ src/main/java/com/principal/miapi/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
     info = @Info(
         title = "Api de Usuarios",
-        description = "Esta api esta desarrollada por el propietario de la cuenta de Github",
+        description = "Esta api esta desarrollada por el propietario de la cuenta de Github, tiene incluido spring security mediante autenticación basica, es decir mediante un usuario y contraseña",
         contact = @Contact(
             name = "Brayan Ruiz Marreros",
             url = "https://github.com/Brayanruiz32"
